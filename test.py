56182cdab22f95590f78b18fa7ab17bd11fb0f77
Victor Xaiver <143952198+VictorhpXavier@users.noreply.github.com>
2024-07-20 11:31:24 +0000

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Load your datasets
-reddit_df = pd.read_csv('../Dataset/Reddit_Data.csv')
-twitter_df = pd.read_csv('../Dataset/Twitter_Data.csv')
-
-# Print the columns to inspect
-print("Reddit DataFrame columns:", reddit_df.columns)
-print("Twitter DataFrame columns:", twitter_df.columns)
